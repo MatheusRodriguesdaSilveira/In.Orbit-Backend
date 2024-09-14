a79852a158bc4146cbabdbca13ea4c705c18e74f
MatheusRodriguesdaSilveira <matheusrsplay1996@gmail.com>
2024-09-14 13:24:07 -0300

    fix: deploy project backend

---
+++ src/http/server.ts
@@ -0,0 +1,27 @@
+import fastify from 'fastify'
+import fastifyCors from '@fastify/cors'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import { createGoalRoute } from './routes/create-goal'
+import { createGoalCompletionRoute } from './routes/create-goal-completion'
+import { getWeekSummaryRoute } from './routes/get-week-summary'
+import { getWeekPendingGoalsRoute } from './routes/get-week-pending-goals'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.register(fastifyCors, { origin: '*' })
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.register(createGoalRoute)
+app.register(createGoalCompletionRoute)
+app.register(getWeekSummaryRoute)
+app.register(getWeekPendingGoalsRoute)
+
+app.listen({ port: 3333 }).then(() => {
+  console.log('HTTP server running!')
+})
